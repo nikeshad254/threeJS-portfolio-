40de0683d53966f2987e5a0c1f16a2c13be524ce
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-11-12 18:11:55 +0545

    added movement to  bird, plane and sky, a lot of eventlistners used and maths with position and rotation*

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,11 +1,19 @@
 import { useGLTF } from "@react-three/drei";
 
 import skyScene from "../assets/3d/sky.glb";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
-function Sky() {
+function Sky({ isRotating, ...props }) {
+  const skyRef = useRef();
   const sky = useGLTF(skyScene);
+
+  useFrame((_, delta) => {
+    if (isRotating) skyRef.current.rotation.y += 0.15 * delta;
+  });
+
   return (
-    <mesh>
+    <mesh ref={skyRef}>
       <primitive object={sky.scene} />
     </mesh>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
 import Bird from "../models/Bird";
@@ -13,6 +13,8 @@ import { Plane } from "../models/Plane";
 }
 
 function Home() {
+  const [isRotating, setIsRotating] = useState(false);
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
@@ -27,13 +29,32 @@ function Home() {
     return [screenScale, screenPosition, rotation];
   };
 
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+  const [curentStage, setCurrentStage] = useState(1); // [1, 2, 3, 4
   const [islandScale, islandPosition, islandRotation] =
     adjustIslandForScreenSize();
 
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
+
   return (
-    <section className="w-full h-screen relative">
+    <section className="w-full h-screen relative ">
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        } `}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
@@ -46,15 +67,23 @@ function Home() {
           />
 
           <Bird />
-          <Sky />
+          <Sky isRotating={isRotating} />
 
           <Island
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
             position={islandPosition}
+            rotation={[0.1, 4.7077, 0]}
             scale={islandScale}
-            rotation={islandRotation}
           />
 
-          <Plane />
+          <Plane
+            isRotating={isRotating}
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0, 20, 0]}
+          />
         </Suspense>
       </Canvas>
     </section>
