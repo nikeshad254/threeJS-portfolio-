cf503da41ba3b7aa0f625bf92bfa6cb2d57be24a
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-11-13 09:31:07 +0545

    alert made and added to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,8 @@ import emailjs from "@emailjs/browser";
 import { Canvas } from "@react-three/fiber";
 import Fox from "../models/Fox";
 import Loader from "../components/Loader";
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 function Contact() {
   const formRef = useRef(null);
@@ -14,6 +16,8 @@ function Contact() {
 
   const [isLoading, setIsLoading] = useState(false);
   const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const { alert, showAlert, hideAlert } = useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -38,8 +42,11 @@ function Contact() {
       )
       .then((result) => {
         setIsLoading(false);
-        //TODO: Show success message
-        //TODO: Hide an Alert
+        showAlert({
+          show: true,
+          text: "Message sent successfully!",
+          type: "success",
+        });
 
         setTimeout(() => {
           setCurrentAnimation("idle");
@@ -50,7 +57,16 @@ function Contact() {
         setIsLoading(false);
         console.log(error);
         setCurrentAnimation("idle");
-        //TODO: Show error message
+        showAlert({
+          show: true,
+          text: "I didnt receive your message. Please try again!",
+          type: "danger",
+        });
+      })
+      .finally(() => {
+        setTimeout(() => {
+          hideAlert();
+        }, [3000]);
       });
   };
   const handleFocus = () => setCurrentAnimation("walk");
@@ -58,6 +74,8 @@ function Contact() {
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
