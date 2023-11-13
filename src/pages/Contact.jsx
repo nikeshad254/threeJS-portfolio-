9bf646b382d7b8046eb8e375571432c512f80cfa
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-11-13 08:16:13 +0545

    contact form added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,92 @@
+import { useRef, useState } from "react";
+
 function Contact() {
-  return <div>Contact</div>;
+  const formRef = useRef(null);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+  };
+  const handleFocus = (e) => {};
+  const handleBlur = (e) => {};
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
+          <label htmlFor="name" className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="John"
+              required
+              className="input"
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label htmlFor="email" className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              placeholder="john@gmail.com"
+              required
+              className="input"
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label htmlFor="message" className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              rows={4}
+              name="message"
+              placeholder="Let me know how I can help you!"
+              required
+              className="textarea"
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            disabled={isLoading}
+          >
+            {isLoading ? "Sending..." : "Send Message"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
